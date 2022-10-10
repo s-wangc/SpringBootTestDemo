2601587893543f5406ce33b0fbaf6cd34b5ee51b
wangc <1969877890@qq.com>
2022-10-10 12:59:33 +0800

    初始化项目

---
+++ src/main/java/com/wc/test/SpringBootTestDemoApplication.java
@@ -0,0 +1,13 @@
+package com.wc.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTestDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wc/test/SpringBootTestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wc.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
